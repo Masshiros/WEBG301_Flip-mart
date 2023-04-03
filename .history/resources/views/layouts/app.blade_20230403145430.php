ea1bbcf6d6bba016fde86982e792863571ce9722
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2023-04-03 14:58:59 +0700

    Add favicon of the website

--- .history/resources/views/layouts/app.blade_20230403145430.php
+++ .history/resources/views/layouts/app.blade_20230403145430.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <!-- Favicon -->
-    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
