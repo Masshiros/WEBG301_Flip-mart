e36e8df6203893f5e2b1e68207ba25756e18910c
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2023-04-17 11:32:06 +0700

    Update some link

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -343,9 +343,9 @@ Flip Mart Home
                                                     <div class="action">
                                                         <ul class="list-unstyled">
                                                             <li class="add-cart-button btn-group">
-                                                                <button data-toggle="tooltip"
+                                                                <button 
                                                                     class="btn btn-primary icon" type="button"
-                                                                    title="Add Cart"> <i
+                                                                    title="Add Cart" data-toggle="modal" data-target="#exampleModal" id="{{$product->id}}" onclick="productView(this.id)"> <i
                                                                         class="fa fa-shopping-cart"></i> </button>
                                                                 <button class="btn btn-primary cart-btn"
                                                                     type="button">Add to cart</button>
@@ -443,9 +443,9 @@ Flip Mart Home
                                                     <div class="action">
                                                         <ul class="list-unstyled">
                                                             <li class="add-cart-button btn-group">
-                                                                <button data-toggle="tooltip"
+                                                                <button 
                                                                     class="btn btn-primary icon" type="button"
-                                                                    title="Add Cart"> <i
+                                                                    title="Add Cart" data-toggle="modal" data-target="#exampleModal" id="{{$product->id}}" onclick="productView(this.id)"> <i
                                                                         class="fa fa-shopping-cart"></i> </button>
                                                                 <button class="btn btn-primary cart-btn"
                                                                     type="button">Add to cart</button>
