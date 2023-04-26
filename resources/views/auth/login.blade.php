43b86ed030f710f8a889d3b35f97827c18aedf90
Phạm Đức Anh <ducanh040202003@gmail.com>
2023-04-26 11:11:38 +0700

    Fix frontend Google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,13 +15,13 @@
 	<div class="container">
 		<div class="sign-in-page">
 			<div class="row">
-				<!-- Sign-in -->			
+				<!-- Sign-in -->
 <div class="col-md-6 col-sm-6 sign-in">
 	<h4 class="">Sign in</h4>
 	<p class="">Hello, Welcome to your account.</p>
 	<div class="social-sign-in outer-top-xs">
 		<a href="#" class="facebook-sign-in"><i class="fa fa-facebook"></i> Sign In with Facebook</a>
-		<a href="#" class="twitter-sign-in"><i class="fa fa-twitter"></i> Sign In with Twitter</a>
+		<a href="#" class="google-sign-in"><i class="fa fa-google"></i> Sign In with Google</a>
 	</div>
 
     <form method="POST" action="{{ isset($guard) ? url($guard.'/login') : route('login')  }}">
@@ -29,23 +29,23 @@
 		<div class="form-group">
 		    <label class="info-title" for="email">Email Address <span>*</span></label>
 		    <input type="email" id="email" class="form-control unicase-form-control text-input" name="email">
-			
+
 		</div>
 	  	<div class="form-group">
 		    <label class="info-title" for="exampleInputPassword1">Password <span>*</span></label>
 		    <input type="password" id="password" name="password" class="form-control unicase-form-control text-input"  >
-			
+
 		</div>
 		<div class="radio outer-xs">
 		  	<label>
 		    	<input type="radio" id="remember_me" name="remember" value="option2">Remember me!
 		  	</label>
-              
+
 		  	<a href="{{ route('password.request') }}" class="forgot-password pull-right">Forgot your Password?</a>
-            
+
         </div>
 	  	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Login</button>
-	</form>					
+	</form>
 </div>
 <!-- Sign-in -->
 
@@ -73,7 +73,7 @@
 			</span>
 			@enderror
 		</div>
-   
+
         <div class="form-group">
 		    <label class="info-title" for="phone">Phone Number <span>*</span></label>
 		    <input type="text" name="phone" class="form-control unicase-form-control text-input" id="phone" >
@@ -103,11 +103,11 @@
 		</div>
 	  	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Sign Up</button>
 	</form>
-	
-	
-</div>	
+
+
+</div>
 <!-- create a new account -->			</div><!-- /.row -->
 		</div><!-- /.sigin-in-->
 		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
-@include('frontend.body.brands')  
+@include('frontend.body.brands')
 @endsection
